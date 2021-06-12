bc5c9a4e95c12fdca49590a052cd06168eb1e87c
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-06-12 17:00:30 +0530

    sort_prep

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
 
   containerMovements.innerHTML = "";
 
